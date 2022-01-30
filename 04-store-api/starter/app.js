81ee2b8a4df82b20488060c8b325b98533839fe8
kizadek <48857035+kizadek@users.noreply.github.com>
2022-01-30 19:51:56 +0200

    PRODUCTS

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,10 +1,12 @@
 //console.log('04 Store API')
 require('dotenv').config({path:"./config/config.env"});
+require('express-async-errors');
 const express = require("express");
 const app = express();
 const morgan  = require("morgan");
 
 const connectDB = require('./db/connect')
+const productsRouter = require('./routes/products')
 
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require('./middleware/error-handler')
@@ -15,10 +17,13 @@ app.use(morgan('dev'))
 // routes
 
 app.get('/',(req,res)=>{
-    return  res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
+    return  res.send('<h1>Store API</h1><a href="/api/v1/stors/products">products route</a>');
 })
 
 // products route
+app.use('/api/v1/stors/products',productsRouter);
+
+
 app.use(notFoundMiddleware);
 // error middlewars
 app.use(errorHandlerMiddleware);
@@ -28,7 +33,7 @@ const PORT = process.env.PORT || 3000
 const start = async () =>{
     try {
         //connectDB
-       connectDB(process.env.MONGO_URI);
+      // connectDB(process.env.MONGO_URI);
      //console.log(process.env.MONGO_URI)
       const  server = app.listen(PORT,console.log(`server is runing on port::${PORT}...`));
       process.on('unhandledRejection',err=>{ console.log("ERROR::",err); server.close(()=>process.exit(1))});
