38addb626e870c3594f961c35b5a113d575033db
mupela nsowa <48857035+kizadek@users.noreply.github.com>
2022-02-12 18:10:20 +0200

    init

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,7 +1,18 @@
 const jwt = require("jsonwebtoken");
-
+const CustomAPIError = require("../errors/custom-error");
+const jwt_secret = process.env.JWT_SECRET;
 const login = async (req, res) => {
-  console.log("login");
+  //console.log(jwt_secret);
+  const { username, password } = req.body;
+
+  if (!username || !password)
+    throw new CustomAPIError("name and password are required!", 400);
+  const id = new Date().getDate();
+  //console.log(id);
+  const token = await jwt.sign({ id, username }, jwt_secret, {
+    expiresIn: "30d",
+  });
+  res.status(200).json({ msg: "token created successfully", token });
 };
 
 const dashboard = async (req, res) => {
